5414f469961b32b21c6c743744654c920016649d
Your Name <regarizz@gmail.com>
2025-01-17 14:20:58 +0800

    layanan publik - aduan

---
+++ web/aduan.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="assets/image/logo_noname.png">
+    <title>BMKG Tarakan - Aduan</title>
+    <link rel="stylesheet" href="css/outer.css">
+    <link rel="stylesheet" href="css/pel_publik.css">
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <div class="iframe-container">
+      <iframe 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdLV41Fs6Lszw--B8uybrl6roZ_yXVc76pfmoy2vi1JxskRXw/viewform?embedded=true" 
+        width="640" 
+        height="805" 
+        frameborder="0" 
+        marginheight="0" 
+        marginwidth="0">
+        Loading...
+      </iframe>
+    </div>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
