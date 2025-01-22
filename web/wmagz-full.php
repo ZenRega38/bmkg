3d183efa946cef3394e377a2c43d2c273361822c
AgungGunawan13 <gagung859@gmail.com>
2025-01-22 09:50:01 +0800

    fix bug

--- web/wmagz-full.php
+++ web/wmagz-full.php
@@ -11,8 +11,6 @@
   <link rel="stylesheet" href="css/wmagz_full.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
-  <script src="assets/script/nav.js"></script>
-
 </head>
 <body>
 <?php include 'header.php'; ?>
