470f722137ba6b683b04d8eea5ecfaede4f8931d
deha2626 <dehaadelia26@gmail.com>
2025-01-23 08:47:40 +0800

    video bg

--- web/PelPublik.php
+++ web/PelPublik.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BMKG Tarakan - Services</title>
-    <link rel="stylesheet" href="pelayanan-publik.css">
+    <link rel="stylesheet" href="css/pelayanan-publik.css">
     <link rel="stylesheet" href="css/outer.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
