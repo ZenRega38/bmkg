4513200c97fe5126e3495d81dabc9994ddbd6ec5
deha2626 <dehaadelia26@gmail.com>
2025-01-30 14:01:02 +0800

    permdata

---
+++ web/PermohonanData.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="assets/image/logo_noname.png">
+    <title>BMKG Tarakan - Aduan</title>
+    <link rel="stylesheet" href="css/outer.css">
+    <link rel="stylesheet" href="css/pel_publik.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <div class="iframe-container" style="display: flex; justify-content: center;">
+      <iframe 
+        src="https://docs.google.com/forms/d/e/1FAIpQLSdLV41Fs6Lszw--B8uybrl6roZ_yXVc76pfmoy2vi1JxskRXw/viewform?embedded=true" 
+        width="640" 
+        height="805" 
+        frameborder="0" 
+        marginheight="0" 
+        marginwidth="0"
+        >
+        Loading...
+      </iframe>
+    </div>
+    <script src="assets/script/nav.js"></script>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
