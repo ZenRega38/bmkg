3a96f5548556fd3f80813bc52e1bfa90239d8bd2
AgungGunawan13 <gagung859@gmail.com>
2025-01-31 10:06:58 +0800

    gs

--- web/profil.php
+++ web/profil.php
@@ -22,8 +22,6 @@
                 <p>
                     Sejarah pengamatan meteorologi dan geofisika di Indonesia – dimulai pada tahun 1841. diawali dengan pengamatan yang dilakukan secara perorangan oleh Dr. Onnen, Kepala Rumah Sakit di Bogor. Tahun demi tahun kegiatannya berkembang sesuai dengan semakin diperlukannya data hasil pengamatan cuaca dan geofisika.
                     Pada tahun 1866, kegiatan pengamatan perorangan tersebut oleh Pemerintah Hindia Belanda diresmikan menjadi instansi pemerintah dengan nama Magnetisch en Meteorologisch Observatorium atau Observatorium Magnetik dan Meteorologi dipimpin oleh Dr. Bergsma. Pada tahun 1879 dibangun jaringan penakar hujan sebanyak 74 stasiun pengamatan di Jawa. Pada tahun 1902 pengamatan medan magnet bumi dipindahkan dari Jakarta ke Bogor. Pengamatan gempa bumi dimulai pada tahun 1908 dengan pemasangan komponen horisontal seismograf Wiechert di Jakarta, sedangkan pemasangan komponen vertikal dilaksanakan pada tahun 1928. Pada tahun 1912 dilakukan reorganisasi pengamatan meteorologi dengan menambah jaringan sekunder. Sedangkan jasa meteorologi mulai digunakan untuk penerangan pada tahun 1930. Pada masa pendudukan Jepang antara tahun 1942 sampai dengan 1945, nama instansi meteorologi dan geofisika diganti menjadi Kisho Kauso Kusho. 
-                    <br>
-                    Setelah proklamasi kemerdekaan Indonesia pada tahun 1945, instansi tersebut dipecah menjadi dua: Di Yogyakarta dibentuk Biro Meteorologi yang berada di lingkungan Markas Tertinggi Tentara Rakyat Indonesia khusus untuk melayani kepentingan Angkatan Udara. Di Jakarta dibentuk Jawatan Meteorologi dan Geofisika, dibawah Kementerian Pekerjaan Umum dan Tenaga. Pada tanggal 21 Juli 1947 Jawatan Meteorologi dan Geofisika diambil aliholeh Pemerintah Belanda dan namanya diganti menjadi Meteorologisch en Geofisiche Dienst. Sementara itu, ada juga Jawatan Meteorologi dan Geofisika yang dipertahankan oleh Pemerintah Republik Indonesia , kedudukan instansi tersebut di Jl. Gondangdia, Jakarta. Pada tahun 1949, setelah penyerahan kedaulatan negara Republik Indonesia dari Belanda, Meteorologisch en Geofisiche Dienst diubah menjadi Jawatan Meteorologi dan Geofisika dibawah Departemen Perhubungan dan Pekerjaan Umum. Selanjutnya, pada tahun 1950 Indonesia secara resmi masuk sebagai anggota Organisasi Meteorologi Dunia (World Meteorological Organization atau WMO) dan Kepala Jawatan Meteorologi dan Geofisika menjadi Permanent Representative of Indonesia with WMO. Pada tahun 1955 Jawatan Meteorologi dan Geofisika diubah namanya menjadi Lembaga Meteorologi dan Geofisika di bawah Departemen Perhubungan, dan pada tahun 1960 namanya dikembalikan menjadi Jawatan Meteorologi dan Geofisika di bawah Departemen Perhubungan Udara. 
                 </p>
             </div>
 
@@ -31,16 +29,14 @@
                 <h2>Video Profil BMKG Juwata Tarakan</h2>
                  <div class="video-container">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/uep9J0hvjVo?si=In2E7tbpOMjGKVT9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>                </div>
-                <p>
-                    <br><br>
-                    Pada tahun 1965, namanya diubah menjadi Direktorat Meteorologi dan Geofisika, kedudukannya tetap di bawah Departemen Perhubungan Udara. Pada tahun 1972, Direktorat Meteorologi dan Geofisika diganti namanya menjadi Pusat Meteorologi dan Geofisika, suatu instansi setingkat eselon II dibawah Departemen Perhubungan, dan pada tahun 1980 statusnya dinaikkan menjadi suatu instansi setingkat eselon I dengan nama Badan Meteorologi dan Geofisika, dengan kedudukan tetap berada di bawah Departemen Perhubungan. Pada tahun 2002, dengan keputusan Presiden RI Nomor 46 dan 48 tahun 2002, struktur organisasinya diubah menjadi Lembaga Pemerintah Non Departemen (LPND) dengan nama tetap Badan Meteorologi dan Geofisika. Terakhir, melalui Peraturan PresidenNomor 61 Tahun 2008, Badan Meteorologi dan Geofisika berganti nama menjadi Badan Meteorologi, Klimatologi, dan Geofisika (BMKG) dengan status tetap sebagai Lembaga Pemerintah Non Departemen. Pada tanggal 1 Oktober 2009 Undang-Undang Republik Indonesia Nomor 31 Tahun 2009 tentang Meteorologi, Klimatologi dan Geofisika disahkan oleh Presiden Republik Indonesia, Susilo Bambang Yudhoyono. (unduh Penjelasan UU RI Nomor 31 Tahun 2009) Sumber : BMKG Pusat
-                </p>
+
             </div>
        </section>
 
         <section class="section-content" id="struktural">
             <h2>Struktur Organisasi BMKG</h2>
             <div class="struktural-container">
+                <img src="assets/image/diagram-struktural.png" alt="Galeri struktural">
                 <p>
                     Sejarah pengamatan meteorologi dan geofisika di Indonesia – dimulai pada tahun 1841. diawali dengan pengamatan yang dilakukan secara perorangan oleh Dr. Onnen, Kepala Rumah Sakit di Bogor. Tahun demi tahun kegiatannya berkembang sesuai dengan semakin diperlukannya data hasil pengamatan cuaca dan geofisika. 
                     <br>
@@ -52,7 +48,7 @@
         <section class="section-content" id="galeri">
             <h2>Galeri BMKG</h2>
             <div class="gallery">
-                <img src="assets/image/galeri1.jpg" alt="Galeri 1">
+                <img src="assets/image/diagram-struktural.jpg" alt="Galeri 1">
                 <img src="assets/image/galeri2.jpg" alt="Galeri 2">
                 <img src="assets/image/galeri3.jpg" alt="Galeri 3">
                 
