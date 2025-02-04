ce780bcae256ce8ca9ce210dc4ffe648d4a22461
Your Name <regarizz@gmail.com>
2025-02-04 15:01:58 +0800

    loading screen

--- web/header.php
+++ web/header.php
@@ -1,97 +1,92 @@
 <header class="bmkg-header-bottom">
-    <div class="container-header">
-        <div class="dashboard">
-        <div class="logo">
-            <img src="assets/image/LOGO.png" alt="BMKG Logo" />
-        </div>
-        <div class="text-content">
-            <p style="font-size: 64px">BMKG Tarakan</p>
-            <p style="font-size: 24px">
-            Badan Meteorologi, Klimatologi, dan Geofisika
-            </p>
-            <div class="solid-line"></div>
-            <p style="font-size: 34px">
-            Cepat, Tepat, Akurat, Luas, dan Mudah Dipahami
-            </p>
-        </div>
-        </div>
+  <div class="container-header">
+    <div class="dashboard">
+      <div class="logo">
+        <img src="assets/image/LOGO.png" alt="BMKG Logo" />
+      </div>
+      <div class="text-content">
+        <p style="font-size: 64px">BMKG Tarakan</p>
+        <p style="font-size: 24px">Badan Meteorologi, Klimatologi, dan Geofisika</p>
+        <div class="solid-line"></div>
+        <p style="font-size: 34px">Cepat, Tepat, Akurat, Luas, dan Mudah Dipahami</p>
+      </div>
     </div>
+  </div>
 </header>
-
 <header id="nav-menu" aria-label="navigation bar">
-    <div class="container">
-        <nav class="menu">
-            <ul class="menu-bar">
-                <li><a class="nav-link" href="index.php">Beranda</a></li>
-                <li><a class="nav-link" href="profil.php" >Profil</a></li>
-                 <li>
-                    <button
-                        class="nav-link dropdown-btn"
-                        data-dropdown="dropdown3"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                        aria-label="discover"
-                        >
-                       Cuaca
-                        <i class="bx bx-chevron-down" aria-hidden="true"></i>
-                    </button>
-                      <div id="dropdown3" class="dropdown">
-                        <ul role="menu">
-                        <li role="menuitem">
-                            <a class="dropdown-link" href="cuaca.php">Cuaca Wilayah</a>
-                        </li>
-                        <li role="menuitem">
-                            <a class="dropdown-link" href="wisata.php">Cuaca Tempat Wisata</a>
-                        </li>
-                       </ul>
-                      </div>
-                </li>
-                <li>
-                    <button
-                        class="nav-link dropdown-btn"
-                        data-dropdown="dropdown2"
-                        aria-haspopup="true"
-                        aria-expanded="false"
-                        aria-label="discover"
-                        >
-                        Pelayanan Publik
-                        <i class="bx bx-chevron-down" aria-hidden="true"></i>
-                    </button>
-                    <div id="dropdown2" class="dropdown">
-                        <ul role="menu">
-                        <li role="menuitem">
-                            <a class="dropdown-link" href="#">Permintaan Data</a>
-                        </li>
-                        <li role="menuitem">
-                            <a class="dropdown-link" href="wmagz.php">W'magazine</a>
-                        </li>
-                        <li role="menuitem">
-                            <a class="dropdown-link" href="aduan.php">Aduan</a>
-                        </li>
-                        <li role="menuitem">
-                            <a class="dropdown-link" href="kritik-saran.php">Kritik dan Saran</a>
-                        </li>
-                        <li role="menuitem">
-                            <a class="dropdown-link" href="https://ptsp.bmkg.go.id/">PTSP</a>
-                        </li>
-                        <li role="menuitem">
-                            <a class="dropdown-link" href="#">Kontak</a>
-                        </li>
-                        <li role="menuitem">
-                            <a class="dropdown-link" href="pelayanan-publik.php">Selengkapnya</a>
-                        </li>
-                        </ul>
-                    </div>
-                </li>
-            </ul>
-        </nav>
-        <button
-            id="hamburger"
-            aria-label="hamburger"
+  <div class="container">
+    <nav class="menu">
+      <ul class="menu-bar">
+        <li><a class="nav-link" href="beranda.php">Beranda</a></li>
+        <li><a class="nav-link" href="profil.php">Profil</a></li>
+        <li>
+          <button
+            class="nav-link dropdown-btn"
+            data-dropdown="dropdown3"
             aria-haspopup="true"
             aria-expanded="false"
-            >
-            <i class="bx bx-menu" aria-hidden="true"></i>
-        </button>
-    </div>
+            aria-label="discover"
+          >
+            Cuaca
+            <i class="bx bx-chevron-down" aria-hidden="true"></i>
+          </button>
+          <div id="dropdown3" class="dropdown">
+            <ul role="menu">
+              <li role="menuitem">
+                <a class="dropdown-link" href="cuaca.php">Cuaca Wilayah</a>
+              </li>
+              <li role="menuitem">
+                <a class="dropdown-link" href="wisata.php">Cuaca Tempat Wisata</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <button
+            class="nav-link dropdown-btn"
+            data-dropdown="dropdown2"
+            aria-haspopup="true"
+            aria-expanded="false"
+            aria-label="discover"
+          >
+            Pelayanan Publik
+            <i class="bx bx-chevron-down" aria-hidden="true"></i>
+          </button>
+          <div id="dropdown2" class="dropdown">
+            <ul role="menu">
+              <li role="menuitem">
+                <a class="dropdown-link" href="#">Permintaan Data</a>
+              </li>
+              <li role="menuitem">
+                <a class="dropdown-link" href="wmagz.php">W'magazine</a>
+              </li>
+              <li role="menuitem">
+                <a class="dropdown-link" href="aduan.php">Aduan</a>
+              </li>
+              <li role="menuitem">
+                <a class="dropdown-link" href="kritik-saran.php">Kritik dan Saran</a>
+              </li>
+              <li role="menuitem">
+                <a class="dropdown-link" href="https://ptsp.bmkg.go.id/">PTSP</a>
+              </li>
+              <li role="menuitem">
+                <a class="dropdown-link" href="#">Kontak</a>
+              </li>
+              <li role="menuitem">
+                <a class="dropdown-link" href="pelayanan-publik.php">Selengkapnya</a>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </nav>
+    <button
+      id="hamburger"
+      aria-label="hamburger"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      <i class="bx bx-menu" aria-hidden="true"></i>
+    </button>
+  </div>
 </header>
